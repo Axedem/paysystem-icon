b63f8e43a5c3cbc54b8cdd4c49615524e4471abb
Andrew Ax. /Home <axedem@ya.ru>
2020-05-15 17:52:34 +0300

    add mono icons

--- index.js
+++ index.js
@@ -1,40 +1,92 @@
-import SVG from 'react-inlinesvg';
-import React from 'react';
+"use strict";
 
-class Icon extends React.Component {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
 
-  preProcessor(code) {
-    code = code.replace(/fill:/g, '')
-    return code.replace(/fill=".*?"/g, '')
+var _reactInlinesvg = _interopRequireDefault(require("react-inlinesvg"));
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var Icon = /*#__PURE__*/function (_React$Component) {
+  _inherits(Icon, _React$Component);
+
+  var _super = _createSuper(Icon);
+
+  function Icon() {
+    _classCallCheck(this, Icon);
+
+    return _super.apply(this, arguments);
   }
 
-  render() {
-    const {source, iconName, fill} = this.props
-    const iconType = fill ? "mono" : "color";
-    if (fill || source) {
-      return <SVG 
-      src={"/svg/" + iconType + "/" + iconName + ".svg"}
-      uniquifyIDs={true}
-      style={{
-        width: this.props.w,
-        height: this.props.h,
-        fill: "#f00",
-      }}
-      preProcessor={this.preProcessor}
-      {...this.props} />
-    } else {
-      return <img
-        style={{
-          width: this.props.w,
-          height: this.props.h,
-        }}
-        className={this.props.className}
-        src={"/svg/color/" + iconName + ".svg"}
-        {...this.props}
-        ></img>
+  _createClass(Icon, [{
+    key: "preProcessor",
+    value: function preProcessor(code) {
+      code = code.replace(/fill:/g, '');
+      return code.replace(/fill=".*?"/g, '');
     }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          source = _this$props.source,
+          iconName = _this$props.iconName,
+          fill = _this$props.fill;
+      var iconType = fill ? "mono" : "color";
 
-  }
-}
+      if (fill || source) {
+        return /*#__PURE__*/_react["default"].createElement(_reactInlinesvg["default"], _extends({
+          src: "../svg/" + iconType + "/" + iconName + ".svg",
+          uniquifyIDs: true,
+          style: {
+            width: this.props.w,
+            height: this.props.h,
+            fill: "#f00"
+          },
+          preProcessor: this.preProcessor
+        }, this.props));
+      } else {
+        return /*#__PURE__*/_react["default"].createElement("img", _extends({
+          style: {
+            width: this.props.w,
+            height: this.props.h
+          },
+          className: this.props.className,
+          src: "../svg/color/" + iconName + ".svg"
+        }, this.props));
+      }
+    }
+  }]);
 
-export default Icon;
+  return Icon;
+}(_react["default"].Component);
+
+var _default = Icon;
+exports["default"] = _default;
