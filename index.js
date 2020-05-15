10de32d8e969eee25b834b0288d0cd30e8a776d7
Andrew Ax. /Home <axedem@ya.ru>
2020-05-15 15:51:32 +0300

    add mono icons

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
+import SVG from 'react-inlinesvg';
 import React from 'react';
-
 
 class Icon extends React.Component {
 
+  preProcessor(code) {
+    code = code.replace(/fill:/g, '')
+    return code.replace(/fill=".*?"/g, '')
+  }
+
   render() {
-    return true
+    const {source, iconName, fill} = this.props
+    const iconType = fill ? "mono" : "color";
+    if (fill || source) {
+      return <SVG 
+      src={"/svg/" + iconType + "/" + iconName + ".svg"}
+      uniquifyIDs={true}
+      style={{
+        width: this.props.w,
+        height: this.props.h,
+        fill: "#f00",
+      }}
+      preProcessor={this.preProcessor}
+      {...this.props} />
+    } else {
+      return <img
+        style={{
+          width: this.props.w,
+          height: this.props.h,
+        }}
+        className={this.props.className}
+        src={"/svg/color/" + iconName + ".svg"}
+        {...this.props}
+        ></img>
+    }
+
   }
 }
-
 
 export default Icon;
